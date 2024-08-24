d5e5a48a1f3dee108ff4d65cae691a7fcfd79892
hann <developer.hann@gmail.com>
2024-08-24 22:35:37 +0900

    🚧 Chore:chores

--- lib/service/metoronom_service.dart
+++ lib/service/metoronom_service.dart
@@ -5,19 +5,29 @@ import 'package:audioplayers/audioplayers.dart';
 class MetoronomService {
   final audio = AudioPlayer();
   Timer? _timer;
-  bool get isPlaying {
-    return _timer?.isActive ?? false;
-  }
+  bool isPlaying = false;
+
+  // bool get isPlaying {
+  //   return _timer?.isActive ?? false;
+  // }
 
   Future play(int bpm) async {
     final intervalMilSec = 60000 ~/ bpm;
-    stop();
-    _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
+    isPlaying = true;
+    while (isPlaying) {
+      await Future.delayed(Duration(milliseconds: intervalMilSec));
+
       audio.play(AssetSource("audio/wood.wav"));
-    });
+      // }
+      // stop();
+      // _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
+      //   audio.play(AssetSource("audio/wood.wav"));
+      // });
+    }
   }
 
   void stop() {
+    isPlaying = false;
     _timer?.cancel();
     _timer == null;
   }
