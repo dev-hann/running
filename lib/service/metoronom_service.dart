52e81fcc7c80814fa988644c3f6153edabc57582
hann <developer.hann@gmail.com>
2024-08-26 21:13:39 +0900

    🎨 Feat: reliable timer

--- lib/service/metoronom_service.dart
+++ lib/service/metoronom_service.dart
@@ -1,28 +1,28 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:just_audio/just_audio.dart';
-
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:reliable_interval_timer/reliable_interval_timer.dart';
 
 class MetoronomService {
-  final audio = AudioPlayer();
-  Timer? _timer;
+  ReliableIntervalTimer? _timer;
 
   bool get isPlaying {
-    return _timer?.isActive ?? false;
+    return _timer?.isRunning ?? false;
   }
 
   Future play(int bpm) async {
     final intervalMilSec = 60000 ~/ bpm;
     stop();
-    _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
-      SystemSound.play(SystemSoundType.click);
-    });
+    ReliableIntervalTimer(
+      interval: Duration(milliseconds: intervalMilSec),
+      callback: (elapsedMilliseconds) {
+        SystemSound.play(SystemSoundType.click);
+      },
+    );
   }
 
   void stop() {
-    _timer?.cancel();
+    _timer?.stop();
     _timer == null;
   }
 }
