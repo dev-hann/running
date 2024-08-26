c3909cd18adc3792b04f5fa68a17750764beb19a
hann <developer.hann@gmail.com>
2024-08-26 20:56:44 +0900

    🚧 Chore: chores

--- lib/service/metoronom_service.dart
+++ lib/service/metoronom_service.dart
@@ -1,35 +1,28 @@
 import 'dart:async';
 
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+
+// import 'package:audioplayers/audioplayers.dart';
 
 class MetoronomService {
   final audio = AudioPlayer();
   Timer? _timer;
-  bool isPlaying = false;
 
-  // bool get isPlaying {
-  //   return _timer?.isActive ?? false;
-  // }
+  bool get isPlaying {
+    return _timer?.isActive ?? false;
+  }
 
   Future play(int bpm) async {
     final intervalMilSec = 60000 ~/ bpm;
-    isPlaying = true;
-    // while (isPlaying) {
-    //   await Future.delayed(Duration(milliseconds: intervalMilSec));
-    //   audio.play(AssetSource("audio/wood.wav"));
-    //   }
     stop();
     _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
-      print("###");
-      // audio.play(AssetSource("audio/wood.wav"));
-      SystemSound.play(SystemSoundType.click);
+      audio.setAsset("assets/audio/wood.wav").then((_) {
+        audio.play();
+      });
     });
-    // }
   }
 
   void stop() {
-    isPlaying = false;
     _timer?.cancel();
     _timer == null;
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,18 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
+import audio_service
+import audio_session
 import geolocator_apple
+import just_audio
 import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
