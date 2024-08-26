03bdc76d4bf475a01bc42c130330f4e7771dc756
hann <developer.hann@gmail.com>
2024-08-26 20:40:31 +0900

    🚧 Chore: chores

--- lib/service/metoronom_service.dart
+++ lib/service/metoronom_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
 
 class MetoronomService {
   final audio = AudioPlayer();
@@ -14,16 +15,17 @@ class MetoronomService {
   Future play(int bpm) async {
     final intervalMilSec = 60000 ~/ bpm;
     isPlaying = true;
-    while (isPlaying) {
-      await Future.delayed(Duration(milliseconds: intervalMilSec));
-
-      audio.play(AssetSource("audio/wood.wav"));
-      // }
-      // stop();
-      // _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
-      //   audio.play(AssetSource("audio/wood.wav"));
-      // });
-    }
+    // while (isPlaying) {
+    //   await Future.delayed(Duration(milliseconds: intervalMilSec));
+    //   audio.play(AssetSource("audio/wood.wav"));
+    //   }
+    stop();
+    _timer = Timer.periodic(Duration(milliseconds: intervalMilSec), (_) {
+      print("###");
+      // audio.play(AssetSource("audio/wood.wav"));
+      SystemSound.play(SystemSoundType.click);
+    });
+    // }
   }
 
   void stop() {
